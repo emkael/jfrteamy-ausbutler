287ea6575c67e540f61ca930a8535d982409c232
emkael <emkael@tlen.pl>
2017-01-06 01:06:17 +0100

    Goniec client

---
+++ ausbutler/goniec.py
@@ -0,0 +1,14 @@
+from .tour_config import Constants
+import socket
+
+class Goniec(object):
+    def __init__(self, config):
+        self.config = config
+
+    def send(self, files=[]):
+        if self.config['enabled']:
+            content_lines = [Constants.path] + files + ['bye', '']
+            goniec = socket.socket()
+            goniec.connect((self.config['host'], self.config['port']))
+            goniec.sendall('\n'.join(content_lines))
+            goniec.close()
