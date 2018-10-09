5a4595fa3dc1e20a33dfba251a4b1eb9be34c5ae
emkael <emkael@tlen.pl>
2018-10-10 01:50:31 +0200

    Config directory lookup in current directory first

--- ausbutler/config.py
+++ ausbutler/config.py
@@ -5,6 +5,9 @@ import __main__
 
 
 def load_config(filename):
-    return json.load(
-        open(path.join(path.dirname(__main__.__file__),
-                       'config', filename + '.json')))
+    possible_directories = ['.', path.dirname(__main__.__file__)]
+    for directory in possible_directories:
+        if path.exists(path.join(directory, 'config')):
+            return json.load(
+                open(path.join(directory, 'config',
+                               filename + '.json')))
